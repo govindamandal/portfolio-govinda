ef4b2cb039de89666bd23887d82d26bc5bbf8369
Govinda Mandal <govinda4india@gmail.com>
2023-05-28 08:20:48 +0530

    Created Skill and Project page in the frontend

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
@@ -11,7 +12,7 @@ class ProjectController extends Controller
      * @return Response
      */
     public function index(Request $request) {
-        return 'Project';
+        return Inertia::render('Projects/Index');
     }
 
     /**
